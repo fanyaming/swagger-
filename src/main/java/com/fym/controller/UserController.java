1caf83928aaaac6ba339533251cfc0f4bba9a821
fan <643463659@qq.com>
2020-04-21 16:39:27 +0800

    swagger学习

---
+++ src/main/java/com/fym/controller/UserController.java
@@ -0,0 +1,68 @@
+package com.fym.controller;
+
+import com.fym.domain.User;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+@RestController
+@RequestMapping(value = "/users")
+@Api(value = "用户相关接口")
+public class UserController {
+
+    static Map<Long, User> users = Collections.synchronizedMap(new HashMap<Long, User>());
+
+    static {
+        User user = new User();
+        user.setId(1L);
+        user.setName("aa");
+        users.put(1L, user);
+    }
+
+    @ApiOperation(value = "获取所有用户列表", notes = "")
+    @RequestMapping(value = {""}, method = RequestMethod.GET)
+    public List<User> getUserList() {
+        List<User> r = new ArrayList<User>(users.values());
+        return r;
+    }
+
+    @ApiOperation(value = "创建新的用户", notes = "根据User对象创建用户")
+    @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    @RequestMapping(value = "", method = RequestMethod.POST)
+    public String postUser(@RequestBody User user) {
+        users.put(user.getId(), user);
+        return "success";
+    }
+
+    @ApiOperation(value = "获取用户详细信息", notes = "根据url的id来获取用户详细信息")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public User getUser(@PathVariable Long id) {
+        return users.get(id);
+    }
+
+    @ApiOperation(value = "更新用户详细信息", notes = "根据url的id来指定更新对象")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long"),
+            @ApiImplicitParam(name = "user", value = "用户详细实体user", required = true, dataType = "User")
+    })
+    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    public String putUser(@PathVariable Long id, @RequestBody User user) {
+        User u = users.get(id);
+        u.setName(user.getName());
+        users.put(id, u);
+        return "success";
+    }
+
+    @ApiOperation(value = "删除已存在的用户", notes = "根据url的id来指定删除对象")
+    @ApiImplicitParam(name = "id", value = "用户ID", required = true, dataType = "Long")
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public String deleteUser(@PathVariable Long id) {
+        users.remove(id);
+        return "success";
+    }
+}
---
+++ src/main/java/com/fym/domain/User.java
@@ -0,0 +1,15 @@
+package com.fym.domain;
+
+import lombok.*;
+
+/**
+ * Created by lenovo on 2020/4/21.
+ */
+@Setter@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class User {
+    private Long id ;
+    private String name;
+}
